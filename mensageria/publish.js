6ef8711ab104b79b22f81450dd2cf25864c8b6f3
Rodney Neville <heptarodney@creasp.org.br>
2024-08-07 09:50:30 -0300

    atauali

--- mensageria/publish.js
+++ mensageria/publish.js
@@ -1,31 +1,36 @@
-// publish.js
+// Importar a biblioteca amqplib
 const amqp = require('amqplib');
 
 async function publishMessage() {
-    const url = 'amqp://user:password@localhost';
-    const queue = 'minhaFila';
-    const message = 'Hello, RabbitMQ!';
+    const amqpUrl = 'amqp://admin:12345@localhost/lab_vhost'; // URL de conexão incluindo o vhost
 
     try {
-        const conn = await amqp.connect(url);
-        const channel = await conn.createChannel();
+        // Conectar ao servidor RabbitMQ
+        const connection = await amqp.connect(amqpUrl);
+        const channel = await connection.createChannel();
 
+        const queue = 'orders';  // Nome da fila
+        const message = 'Hello, RabbitMQ!';  // Mensagem a ser enviada
+
+        // Garantir que a fila existe antes de tentar publicar
         await channel.assertQueue(queue, {
-            durable: true
+            durable: true  // Fila persistente
         });
 
         // Enviar uma mensagem para a fila
         channel.sendToQueue(queue, Buffer.from(message));
-        console.log(`Mensagem enviada: ${message}`);
+        console.log("Mensagem enviada:", message);
 
-        // Fechar a conexão e o canal após um curto atraso
+        // Fechar a conexão e sair
         setTimeout(() => {
-            channel.close();
-            conn.close();
+            connection.close();
+            process.exit(0);
         }, 500);
     } catch (error) {
-        console.error('Erro ao publicar mensagem:', error);
+        console.error("Erro ao publicar mensagem:", error);
+        process.exit(1);
     }
 }
 
+// Executar a função de publicar mensagem
 publishMessage();
